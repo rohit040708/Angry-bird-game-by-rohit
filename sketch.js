8780af9dacd06ef42f07a2439f426ea93c279d74
samar <67219193+rohit040708@users.noreply.github.com>
2021-01-08 16:30:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,14 +7,18 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var score=0;
+var birds=[];
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+   // backgroundImg = loadImage("sprites/bg.png");
+    getbackground();
 }
 
 function setup(){
+    
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
@@ -39,24 +43,37 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(200,50);
+    bird2 = new Bird(150,170);
+    bird3 = new Bird(100,170);
+    bird4 = new Bird(50,170);
+    birds.push(bird4);
+    birds.push(bird3);
+    birds.push(bird2);
+    birds.push(bird);
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
+    if(backgroundImg)
     background(backgroundImg);
+    textSize(35);
+    fill("white");
+    text("Score: "+score,width-300,50);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -64,6 +81,10 @@ function draw(){
     log5.display();
 
     bird.display();
+    bird2.display();
+    bird3.display();
+    bird4.display();
+
     platform.display();
     //log6.display();
     slingshot.display();    
@@ -71,18 +92,43 @@ function draw(){
 
 function mouseDragged(){
     if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+        Matter.Body.setPosition(birds[birds.length-1].body, {x: mouseX , y: mouseY});
+        Matter.Body.applyForce(birds[birds.length-1].body,birds[birds.length-1].body.position,{x:5, y:-5});
+        return false;
     }
 }
 
 
 function mouseReleased(){
+   
     slingshot.fly();
+    birds.pop();
     gameState = "launched";
+    return false;
 }
 
 function keyPressed(){
     if(keyCode === 32){
-       // slingshot.attach(bird.body);
+        if(birds.length>0)
+    gameState="onSling";
+    birds[birds.length-1].body.trajectory=[];
+    birds[birds.length-1].body.visiblity=255;
+    Matter.Body.setPosition(birds[birds.length-1].body, {x: 200 , y: 50 });
+        slingshot.attach(birds[birds.length-1].body);
     }
+}
+
+async function getbackground(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/America/Adak");
+    var responsejson = await response.json();
+    var datetime=responsejson.datetime;
+    var hour = datetime.slice(11,13)
+    if(hour>=07&&hour<=19){
+        bg="sprites/bg.png"
+    }
+    else{
+        bg="sprites/bg2.jpg"
+    }
+    backgroundImg=loadImage(bg);
+    console.log(hour);
 }
